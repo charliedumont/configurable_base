82314d616693e9b7ea6cc142b60b8f0492af663f
Charlie Dumont <charlie.e.dumont@gmail.com>
2017-10-22 06:03:44 -0400

    lint updates

--- pbi/errors.py
+++ pbi/errors.py
@@ -1,41 +1,53 @@
+""" Personal custom error handling """
 import logging
-import ndb_json
 import traceback
 
-error_dict = {}
+import ndb_json
+
+ERROR_DICT = {}
 
 class CustomException(Exception):
-    def __init__(self, error_code=None, details=None, response=None):
+    """ Our own base custom exception """
+    def __init__(self, error_code=None, details=None, response=None, *args, **kwargs):
         self.__error_code = error_code
         self.__details = details
         self.__response = response
+        super(CustomException, self).__init__(*args, **kwargs)
 
     def __str__(self):
-         return repr(self.__details)
+        return repr(self.__details)
 
     @property
     def error_code(self):
+        """ Error_code property"""
         return self.__error_code
 
     @property
     def details(self):
+        """ Details property"""
         return self.__details
 
     @property
     def response(self):
+        """ Response property"""
         return self.__response
 
-    def respond(self, response=None, status='500'):
+    def respond(self, response=None, status=500):
+        """ Mechanism for turning custom exception into
+        restful response """
         if response is None:
             response = self.response
-        e = ErrorReturn(response, error_code=self.error_code,
-                        details=self.details)
+        err = ErrorReturn(
+            response,
+            error_code=self.error_code,
+            details=self.details)
         logging.info("CE.r")
         logging.info(self.details)
-        e.handle_response(status)
+        err.handle_response(status)
 
 
 class ErrorReturn(object):
+    """ Class that handles html response """
     def __init__(self, response, error_code=None, details=None):
         self.response = response
         self.__error_code = error_code
@@ -43,10 +55,12 @@ class ErrorReturn(object):
 
     @property
     def error_package(self):
+        """ Error package property
+        handles formatting based on what was set """
         return_package = {}
         if self.__error_code:
             return_package['code'] = self.__error_code
-            return_package['description'] = error_dict[self.__error_code]
+            return_package['description'] = ERROR_DICT[self.__error_code]
         else:
             return_package['code'] = 'N/A'
             return_package['description'] = 'N/A'
@@ -55,31 +69,32 @@ class ErrorReturn(object):
             return_package['details'] = self.__details
         return return_package
 
-    def handle_response(self, status):
+    def handle_response(self, status=None):
+        """ General repsonse handler """
         logging.info("ER.h_r")
         logging.info(self.__details)
         if int(status) == 200:
-            self.handle_200()
+            self._handle_200()
         elif int(status) == 400:
-            self.handle_400()
+            self._handle_400()
         elif int(status) == 401:
-            self.handle_401()
+            self._handle_401()
         elif int(status) == 403:
-            self.handle_403()
+            self._handle_403()
         elif int(status) == 404:
-            self.handle_404()
+            self._handle_404()
         elif int(status) == 500:
-            self.handle_500()
+            self._handle_500()
 
     # Default to walk ourselves out of some of this error handling
-    def handle_200(self):
+    def _handle_200(self):
         error_info = self.error_package
         self.response.headers.add_header("Access-Control-Allow-Origin", "*")
         self.response.headers['content-type'] = "application/json"
         self.response.write(ndb_json.dumps(error_info))
         self.response.set_status(200)
 
-    def handle_400(self):
+    def _handle_400(self):
         logging.info("400 in our own method")
         self.response.headers.add_header("Access-Control-Allow-Origin", "*")
         error_info = self.error_package
@@ -88,7 +103,7 @@ class ErrorReturn(object):
         self.response.set_status(400)
         traceback.print_stack()
 
-    def handle_401(self):
+    def _handle_401(self):
         logging.info("401 in our own method")
         error_info = self.error_package
         self.response.headers['content-type'] = "application/json"
@@ -96,7 +111,7 @@ class ErrorReturn(object):
         self.response.set_status(401)
         traceback.print_stack()
 
-    def handle_403(self):
+    def _handle_403(self):
         logging.info("403 in our own method")
         self.response.headers.add_header("Access-Control-Allow-Origin", "*")
         error_info = self.error_package
@@ -105,7 +120,7 @@ class ErrorReturn(object):
         self.response.set_status(403)
         traceback.print_stack()
 
-    def handle_404(self):
+    def _handle_404(self):
         logging.info("404 in our own method")
         self.response.headers.add_header("Access-Control-Allow-Origin", "*")
         error_info = self.error_package
@@ -114,11 +129,11 @@ class ErrorReturn(object):
         self.response.set_status(404)
         traceback.print_stack()
 
-    def handle_500(self):
+    def _handle_500(self):
         logging.info("500 in our own method")
+        self.response.headers.add_header("Access-Control-Allow-Origin", "*")
         error_info = self.error_package
         self.response.headers['content-type'] = "application/json"
         self.response.write(ndb_json.dumps(error_info))
         self.response.set_status(500)
         traceback.print_stack()
-
